25294507ccdbf834dd6c5777dbefbce268403da4
elmysf <elmysufiandy09@gmail.com>
2023-09-14 19:22:24 +0700

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Sufiandy Elmy on 14/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
